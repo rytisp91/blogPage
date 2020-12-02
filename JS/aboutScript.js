89f19ffb1cc63d1410eaf5deb1288127ff856852
rytisp91 <68050841+rytisp91@users.noreply.github.com>
2020-12-02 10:57:58 +0200

    login started

---
+++ JS/aboutScript.js
@@ -0,0 +1,24 @@
+// GETS
+const aside = document.getElementById('aside')
+const menuAside = document.getElementById('menuAside')
+const closeAsideBtn = document.getElementsByClassName("close")[0]
+
+// VARS
+let asideTools = {
+    openAside: () => {
+        aside.style.display = "block"
+    },
+    closeAside: (event) => {
+        if (event.target == aside) {
+            aside.style.display = "none";
+        }
+    },
+    closeAsideByBtn: () => {
+        aside.style.display = "none"
+    }
+}
+
+// LISTENERS
+window.addEventListener('click', asideTools.closeAside)
+menuAside.addEventListener('click', asideTools.openAside)
+closeAsideBtn.addEventListener(`click`, asideTools.closeAsideByBtn)
